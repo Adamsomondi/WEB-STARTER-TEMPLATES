23aa1a75e98d0353265ff585d59c83445a4d73a0
adams nyandiga <mustafajohnson123@gmail.com>
2025-03-07 02:44:13 +0300

    laravel-project-template

---
+++ laravel-project/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to Laravel</title>
+</head>
+<body>
+<h1>Welcome to Laravel my nigga</h1>
+<p>This is a simple starter template for a Laravel project.</p>
+</body>
+</html>
